71b4a8c666ac40ba2a053d52c41c8b38f9e35bf8
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-12-16 18:00:09 +0100

    fix travisCi build issue

--- dev-target/build.sh
+++ dev-target/build.sh
@@ -11,7 +11,7 @@
 set +ev
 
 export CFLAGS='-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53'
-export RUSTFLAGS='-C linker=aarch64-elf-gcc.exe -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link.ld'
+export RUSTFLAGS='-C linker=aarch64-elf-gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link.ld'
 
 # if there is any 1 command line argument given don't set the CC/AR variables
 # this is required to be set only when building locally, but not on travis
