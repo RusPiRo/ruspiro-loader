630220d4faad17bf2c8a52f09653aa1dc11a64a0
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-12-20 23:23:32 +0100

    minor fixes for stable travis build

--- test-kernel/build.sh
+++ test-kernel/build.sh
@@ -1,56 +1,54 @@
 #!/bin/sh
-
+#*********************************************************************************
+# script to build the kernel binary ready to be pushed to Raspberry Pi
+#*********************************************************************************
 set +ev
 
 if [ $# -eq 0 ] 
     then 
-        echo "not enough parameter given"
+        echo "provide the target architecture to build for - 32 or 64"
         exit 1
 fi
 
-# chekc which aarch version to build
 if [ $1 = "64" ]
     then
         # aarch64
-        export CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        export RUSTFLAGS="-C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C link-arg=-T./link64.ld -C opt-level=3 -C debuginfo=0"
-        if [ -z "$3" ]
+        # use the right compiler toolchain prefix when building on travis
+        if [ -z "$2" ]
             then
-                export CC="aarch64-elf-gcc"
-                export AR="aarch64-elf-ar"
-                export RUSTFLAGS="-C linker=aarch64-elf-gcc ${RUSTFLAGS}"
+                _PREFIX=aarch64-elf-
+            else
+                _PREFIX=aarch64-linux-gnu-
         fi
-        cargo xbuild --target aarch64-unknown-linux-gnu --release --all
-        if [ -z "$3" ]
-            then
-                cargo objcopy -- -O binary ./target/aarch64-unknown-linux-gnu/release/kernel ./target/kernel8.img
-                # after build deploy to device using serial port
-                if [ "$2" = "deploy" ]
-                    then
-                        cargo ruspiro-push -k ./target/kernel8.img -p COM5
-                fi
-        fi
+        _CFLAGS="-march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        _RUSTFLAGS="-C linker=${_PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+neon -C link-arg=-nostartfiles -C opt-level=3 -C debuginfo=0 -C link-arg=-T./link64.ld"
+        _TARGET="aarch64-unknown-linux-gnu"
+        _KERNEL="kernel8.img"
 elif [ $1 = "32" ]
     then
         # aarch32
-        export CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
-        export RUSTFLAGS="-C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
-        if [ -z "$3" ]
+        # use the right compiler toolchain prefix when building on travis
+        if [ -z "$2" ]
             then
-                export CC="arm-eabi-gcc"
-                export AR="arm-eabi-ar"
-                export RUSTFLAGS="-C linker=arm-eabi-gcc ${RUSTFLAGS}"
+                _PREFIX=arm-eabi-
+            else
+                _PREFIX=arm-linux-gnueabihf-
         fi
-        cargo xbuild --target armv7-unknown-linux-gnueabihf --release --bin kernel --target-dir ./target/
-        if [ -z "$3" ]
-            then
-                cargo objcopy -- -O binary ./target/armv7-unknown-linux-gnueabihf/release/kernel ./target/kernel7.img
-                # after build deploy to device using serial port
-                if [ "$2" = "deploy" ]
-                    then
-                        cargo ruspiro-push -k ./target/kernel7.img -p COM5
-                fi
-        fi
+        _CFLAGS="-mfpu=neon-fp-armv8 -mfloat-abi=hard -march=armv8-a -Wall -O3 -nostdlib -nostartfiles -ffreestanding -mtune=cortex-a53"
+        _RUSTFLAGS="-C linker=${_PREFIX}gcc -C target-cpu=cortex-a53 -C target-feature=+strict-align,+a53,+fp-armv8,+v8,+vfp3,+d16,+thumb2,+neon -C link-arg=-nostartfiles -C link-arg=-T./link32.ld -C opt-level=3 -C debuginfo=0"
+        _TARGET="armv7-unknown-linux-gnueabihf"
+        _KERNEL="kernel7.img"
 else
-    echo 'provide the archtitecture to be build. Use either "build.sh 32" or "build.sh 64" followed by "deploy" if you like to deploy to the device'
+    echo 'provide the archtitecture to be build. Use either "build.sh 32" or "build.sh 64".'
+    exit 1
 fi
+
+export CFLAGS="${_CFLAGS}"
+export RUSTFLAGS="${_RUSTFLAGS}"
+export CC="${_PREFIX}gcc"
+export AR="${_PREFIX}ar"
+export TARGET="${_TARGET}"
+export KERNEL="${_KERNEL}"
+
+cargo xbuild --target ${_TARGET} --release
+cargo objcopy -- -O binary ./target/${_TARGET}/release/kernel ./target/${_KERNEL}
